9f1703edf5537b71c3e787da56172824ab3a1d3f
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-12-05 23:22:39 +0530

    update database convex

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,14 +3,25 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const {toggleDarkMode} = useTheme();
+  
+  // const todos = useQuery(api.todos.getTodos);
+  // console.log(todos);
+  // const addTodos =  useMutation(api.todos.addTodo);
+  // const clearAllTodos =  useMutation(api.todos.clearAllTodos);
   return (
     <View
       style={styles.container}
     >
       <Text style={styles.text}>Kuntal Ojha !</Text>
-      <TouchableOpacity onPress={toggleDarkMode}>
-        <Text>Toggle the mode</Text>
-      </TouchableOpacity>
+        <TouchableOpacity onPress={toggleDarkMode}>
+          <Text>Toggle the mode</Text>
+        </TouchableOpacity>
+        {/* <TouchableOpacity onPress={() => addTodos({text:"New Todo from Expo"})}>
+          <Text>Add Todo</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => clearAllTodos()}>
+          <Text>Clear All Todos</Text>
+        </TouchableOpacity> */}
     </View>
   );
 }
