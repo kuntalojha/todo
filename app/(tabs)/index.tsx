1a6d31aac98a299246b5c3130b91541f691eedf1
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-12-05 22:19:01 +0530

    update dark mode

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
+import useTheme from "@/hooks/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const {toggleDarkMode} = useTheme();
   return (
     <View
       style={styles.container}
     >
       <Text style={styles.text}>Kuntal Ojha !</Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>Toggle the mode</Text>
+      </TouchableOpacity>
     </View>
   );
 }
